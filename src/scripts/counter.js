6f54726852afc58f49b2eeebed74686c94d13c7b
JacobWay <soarinblue@gmail.com>
2017-01-03 10:58:55 +0800

    add auto change functionality in counter

--- src/scripts/counter.js
+++ src/scripts/counter.js
@@ -1,13 +1,28 @@
 import React, {Component} from "react";
 import {render} from "react-dom";
 
+const counter = (state={value: 0}, action) => {
+    switch (action.type){
+        case "increment":
+            return {value: state.value + 1};
+        case "decrement":
+            return {value: state.value - 1};
+        default:
+            return state;
+    }
+}
+
 class Counter extends Component{
-    state = {value: 0};
+    state = counter(undefined, {});
+
+    dispatch = (action) => {
+        this.setState( (prevState) => {
+            return counter(prevState, action)
+        } );
+    }
 
     increment =  () => {
-        this.setState( (prevState) => ({
-            value: prevState.value + 1
-        }) )
+        this.dispatch({type: "increment"});
     };
 
     decrement = ( () => {
@@ -15,6 +30,17 @@ class Counter extends Component{
             value: prevState.value - 1
         }) )
     });
+
+    autoChange = () => {
+        if(this.timer){
+            clearTimeout(this.timer);
+        }
+        this.timer = setTimeout(this.increment, 1000);
+    }
+
+    componentDidUpdate = () => {
+        this.autoChange();
+    }
 
     render(){
         return(
